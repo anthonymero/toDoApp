2c32a9aca46516f48df9d84d44141425402dc2d0
anthony mero <anthony@gladys.io>
2020-01-18 21:39:06 +0100

    - app html - app component onDrop method - Set css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDoApp';
+
+  tasks = [
+    {
+      id: 1,
+      title: 'task 01',
+      details: 'task 01 details'
+    },
+    {
+      id: 2,
+      title: 'task 2',
+      details: 'task 2 details'
+    },
+    {
+      id: 3,
+      title: 'task 3',
+      details: 'task 3 details'
+    },
+
+  ];
+
+  onDrop(event: CdkDragDrop<any[]>) {
+    console.log(event);
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
+    console.log(this.tasks);
+
+  }
 }
