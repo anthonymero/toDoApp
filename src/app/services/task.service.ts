5b7e19428e6c9931d2624a85c250df2a80b2ada3
anthony mero <anthony@gladys.io>
2020-02-09 18:12:15 +0100

    - Create taskService

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  addTask() {
+
+  }
+
+  deleteTask() {
+
+  }
+
+  editTask() {
+    
+  }
+}
